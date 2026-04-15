86e1790f3720ae614a7a5743498530bb66766b5e
William <admin@bysoft.us>
2026-04-15 11:48:31 -0500

    SDK-JavaScrum

---
+++ JScrumSDK.java
@@ -0,0 +1,11 @@
+//SDK Java
+//V.2 Product Owner importar la Libreria Scanner, Capturar Datos por Teclado y actualizar
+//V.3 Scrum Team : Importar:import javax.swing.JOptionPane; Capturar Datos de entrada y actualizar
+public class JScrumSDK {
+  public static void main(String args[]){
+      int edad=25;
+      String name="William D. Cristancho B.";
+      String rol="ProductoOwner";
+	  System.out.println("Soy el "+rol+" de Cloudpro.."+name);
+  }
+}
